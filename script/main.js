3099de1f88ef4d1bd3deec5a8e5efbcd9ef57c3b
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-12 16:38:08 +0100

    feat: basic project skel
    with adaptive svg favicon
    
    Changes to be committed:
    	new file:   img/ico.svg
    	new file:   index.html
    	new file:   script/main.js
    	new file:   style/main.css

---
+++ script/main.js
@@ -0,0 +1,12 @@
+/**
+ * @file main.js
+ * @author Paul Williams <advanced_solution_delivery@outlook.com>
+ * @version 0.1.0
+ * @date 2025-06-11
+ * @summary main file for calculator project. See ../docs/design for design
+ * documentation
+ */
+
+"use strict";
+
+console.log("main.js properly linked");
