9da29f57e50610da2b06629e047c77d6b08a0fa2
Paul Williams <advanced_solution_delivery@outlook.com>
2025-06-19 12:58:07 +0100

    feat: button event listeners
    script link in html
    
    Changes to be committed:
    	modified:   index.html
    	modified:   scripts/calc.js

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,32 @@
 "use strict";
 
-console.log("Script working!");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach((el) =>
+  el.addEventListener("click", (event) => {
+    if (event.target.classList.contains("num")) {
+      processNumInput(event.target.textContent);
+    }
+    if (event.target.classList.contains("op")) {
+      processOpInput(event.target.id);
+    }
+    if (event.target.id == "cancel") cancel();
+    if (event.target.id == "back-space") backSpace();
+  })
+);
+
+function processNumInput(num) {
+  console.log("Number pressed:", num);
+}
+
+function processOpInput(op) {
+  console.log("Operation pressed:", op);
+}
+
+function cancel() {
+  console.log("Cancel pressed");
+}
+
+function backSpace() {
+  console.log("Backspace pressed");
+}
